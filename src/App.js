a70050c9e8f2c3cc7a39d8c010227facbbb62a7e
reszi123 <61701829+reszi123@users.noreply.github.com>
2021-09-19 16:37:57 +0300

    fix sort and add search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import PostList from './components/PostList';
 import "./styles/App.css"
 import PostForm from './components/PostForm';
 import MySelect from './components/ui/select/MySelect';
+import MyInput from './components/ui/input/MyInput';
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,10 +12,22 @@ function App() {
     ])
 
     const [selectedSort, setSelectedSort] = useState("")
+    const [searchQuery, setSeacrhQuery] = useState("")
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+
+    const sortedPosts = useMemo(() => {
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+    }, [searchQuery, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -22,7 +35,7 @@ function App() {
 
     const sortPosts = (sort) => {
         setSelectedSort(sort);
-        setPosts([[...posts].sort((a, b) => a[sort].localeCompare(b[sort]))])
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
     }
 
     return (
@@ -30,6 +43,11 @@ function App() {
             <PostForm create={createPost} />
             <hr style={{ margin: "15px 0" }} />
             <div>
+                <MyInput
+                    onChange={e => setSeacrhQuery(e.target.value)}
+                    value={searchQuery}
+                    placeholder="Поиск..."
+                />
                 <MySelect
                     value={selectedSort}
                     onChange={sortPosts}
@@ -40,9 +58,9 @@ function App() {
                     ]}
                 />
             </div>
-            {posts.length
+            {sortedAndSearchedPosts.length
                 ?
-                <PostList remove={removePost} posts={posts} title={"Список постов"} />
+                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Список постов"} />
                 :
                 <h1 style={{ textAlign: "center" }}>
                     Посты не найдены!
--- src/components/ui/select/MySelect.jsx
+++ src/components/ui/select/MySelect.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from '../select/MySelect.module.css'
 
 const MySelect = ({ options, defaultValue, value, onChange }) => {
     return (
-        <div>
+        <div className={classes.mySel}>
             <select 
                 value={value}
                 onChange={e => onChange(e.target.value)}
